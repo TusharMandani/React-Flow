2835ac5f25b39a38227190f91e1f9e48bc8368a9
TusharMandani <tusharmandani20@gmail.com>
2024-11-13 23:34:18 +0530

    Add node and edges.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  Background,
+  Controls,
+  applyEdgeChanges,
+  applyNodeChanges,
+  addEdge,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
+const initialEdges = [
+  {
+    id: "1-2",
+    source: "1",
+    target: "2",
+    label: "Software Developer",
+    type: "step",
+  },
+];
+
+const initialNodes = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "Tushar" },
+  },
+  {
+    id: "2",
+    position: { x: 100, y: 100 },
+    data: { label: "Mandani" },
+  },
+];
 
 function App() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100%" }}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
     </div>
   );
 }
